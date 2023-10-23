5392c473dd1eb4571a2a3a51dc44c23eff31c664
3abglil <mks.abogalil666@gmail.com>
2023-10-23 17:12:18 +0300

    app.js created

---
+++ app.js
@@ -0,0 +1,63 @@
+// import supabase from './models/database.js';
+import express from 'express';
+import path from "path";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+
+
+
+
+const app = express();
+
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+// app.use(fileUpload());
+// app.use(logger("common"));
+// app.use(express.json());
+// app.use(session({
+//   secret:'key to sign the cookie',
+// }))
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+import { createClient } from '@supabase/supabase-js'
+
+// const supabaseUrl = process.env.SUPABASE_URL
+const supabaseUrl = "https://hxvloruvdgwljekxwqwh.supabase.co"
+// const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4dmxvcnV2ZGd3bGpla3h3cXdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc5OTE4NDEsImV4cCI6MjAxMzU2Nzg0MX0.0GKNK-i7lszlopu2MSIkWtsiV3nFo4Kd2x_ryPzHeTw"
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+// export default supabase
+
+
+
+  app.get('/', async (req, res) => {
+  //   async function insertData() {
+  //     // const { data, error } = await supabase
+  //     //   .from('users')
+  //     //   .insert({Email:"gello"});
+  //     const { data, error } = await supabase
+  // .from('users')
+  // .select("name")
+    
+  //     if (error) {
+  //       console.error(error);
+  //       return;
+  //     }
+    
+  //     console.log('Data inserted successfully:', data);
+  //   }
+    
+  //   insertData();
+  res.render("admin");
+});
+
+app.listen(8000, () => {
+  console.log('Server listening on port 8000');
+}); 
