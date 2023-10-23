2fe2fb8f8900ac70168a0a3c16283961af17031d
YoussefBakry5 <125702757+YoussefBakry5@users.noreply.github.com>
2023-10-23 20:48:51 +0300

    buttons commit

--- routes/admin.js
+++ routes/admin.js
@@ -5,8 +5,14 @@ const router = Router();
 router.get("/",(req,res)=>{
     res.render("admin")
 });     
-
-
+router.get('/adduser',(req,res)=>{
+    res.render('adduser');
+    
+})
+router.get('/addproduct',(req,res)=>{
+    res.render('addproduct');
+    
+})
 
 
 
