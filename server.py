8f25a7e0baf365526eabcde3cd3a08fe26d9f0e0
3abglil <mks.abogalil666@gmail.com>
2025-02-13 00:00:55 +0200

    port

--- server.py
+++ server.py
@@ -59,8 +59,8 @@ async def transcribe_audio(file: UploadFile = File(...)):
             status_code=500
         )
 
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 8080))  # Render assigns a dynamic port
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8888))  # Render assigns a dynamic port
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
 
