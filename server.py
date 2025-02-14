732469c4e279fe0c4f4409bb4527f2c51a36af26
3abglil <mks.abogalil666@gmail.com>
2025-02-14 17:29:49 +0200

    dep

--- server.py
+++ server.py
@@ -60,3 +60,8 @@ async def transcribe_audio(file: UploadFile = File(...)):
         )
 
 
+import os
+import uvicorn
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port) 
